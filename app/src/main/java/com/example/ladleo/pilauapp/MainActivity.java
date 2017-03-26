3fabda220a50e520008b5a31e6347cb059b100f1
Denis Rwelamila <rwelamila.jr@gmail.com>
2017-03-26 07:19:16 +0300

    display bottom nav icons

--- app/src/main/java/com/example/ladleo/pilauapp/MainActivity.java
+++ app/src/main/java/com/example/ladleo/pilauapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ladleo.pilauapp;
 
-import android.app.Fragment;
 import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,8 +28,6 @@ public class MainActivity extends AppCompatActivity {
                 android.app.FragmentManager fragmentManager = getFragmentManager();
                 android.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-
-
                 fragmentTransaction.add(R.id.contentContainer, new UserProfile(), "user_profile");
                 fragmentTransaction.add(R.id.contentContainer, new NewPost(), "new_post");
                 fragmentTransaction.add(R.id.contentContainer, new Explore(), "explore");
@@ -35,22 +35,24 @@ public class MainActivity extends AppCompatActivity {
                 if (tabId == R.id.action_profile){
 //                    show user profile
                     UserProfile fragment = new UserProfile();
-                    fragmentTransaction.add(R.id.contentContainer, fragment);
+                    fragmentTransaction.replace(R.id.contentContainer, fragment);
+                    fragmentTransaction.addToBackStack(null);
                     fragmentTransaction.commit();
                 }
                 else if(tabId == R.id.action_post){
                     Log.e("Message", "Action profile clicked:"+tabId);
 //                    show the new post activity
                     NewPost fragment = new NewPost();
-                    fragmentTransaction.add(R.id.contentContainer, fragment);
+                    fragmentTransaction.replace(R.id.contentContainer, fragment);
+                    fragmentTransaction.addToBackStack(null);
                     fragmentTransaction.commit();
-
                 }
                 else if(tabId == R.id.action_explore){
                     Log.e("Message", "Action profile clicked:"+tabId);
 //                    show the explore activity
                     Explore fragment = new Explore();
-                    fragmentTransaction.add(R.id.contentContainer, fragment);
+                    fragmentTransaction.replace(R.id.contentContainer, fragment);
+                    fragmentTransaction.addToBackStack(null);
                     fragmentTransaction.commit();
                 }
             }
